c97e7ff21416c8693f97da2b27fdadcaf79101dd
sassius <145041644+sassius@users.noreply.github.com>
2025-04-04 17:59:57 +0530

    added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import {
 
 function About({ language }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen">
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Left Column with Image */}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -32,63 +32,14 @@ const HomePage = ({ language }) => {
       </ParallaxSection>
 
       {/* About Section */}
-      <ParallaxSection className="bg-white">
-        {/* <div id='about' className="max-w-7xl mx-auto px-4 py-24">
-          <h2 className="text-4xl font-bold text-center mb-12">
-            {language === "en" ? "About Us" : "हमारे बारे में"}
-          </h2>
-          <div className="text-lg text-gray-700 max-w-4xl mx-auto space-y-6">
-            {language === "en" ? (
-              <>
-                <p>
-                  "Tatsam" is committed to promoting Hindi reading and writing
-                  among the students of Netaji Subhas University of Technology.
-                  Tatsam represents all students of the university who are
-                  interested in the Hindi language. Our primary aim is to
-                  cultivate a strong inclination towards Hindi within the
-                  university.
-                </p>
-                <p>
-                  We strive to achieve this goal by organizing various
-                  competitions such as debates, poetry writing and recitation,
-                  story writing, and more. In addition to this, we also host
-                  numerous seminars, poetry gatherings, and other events based
-                  on Hindi literature.
-                </p>
-                <p>
-                  Ultimately, Tatsam is not just a group of like-minded students
-                  — it is an emerging ideology rapidly gaining momentum within
-                  the university.
-                </p>
-              </>
-            ) : (
-              <>
-                <p>
-                  "तत्सम" नेताजी सुभाष प्रौद्योगिकी विश्वविद्यालय के छात्रों में
-                  हिंदी वाचन एवं लेखन को प्रोत्साहित करने के लिए प्रतिबद्ध है।
-                  तत्सम विश्वविद्यालय के सभी हिंदी में रुचि रखने वाले छात्रों का
-                  प्रतिनिधित्व करती है। हमारा ध्येय मूलतः विश्वविद्यालय में
-                  हिंदी के प्रति रुझान पैदा करना है।
-                </p>
-                <p>
-                  इस उद्देश्य की पूर्ति हम विभिन्न प्रतियोगिताओं के आयोजन द्वारा
-                  करते है जैसे वाद-विवाद, कविता लेखन एवं पाठन, कहानी लेखन
-                  इत्यादि। इसके अलावा हम हिंदी साहित्य पर आधारित अनेक
-                  संगोष्ठियों, कवि सम्मेलनों आदि का भी आयोजन करते है।
-                </p>
-                <p>
-                  अंततः तत्सम महज़ समान रुचि रखने वाले छात्रों का समूह ही नही
-                  बल्कि विश्वविद्यालय में तेज़ी से उभरती हुई एक विचारधारा है।
-                </p>
-              </>
-            )}
-          </div>
-        </div> */}
+      <ParallaxSection className="bg-purple-50">
+        <div className="max-w-7xl mx-auto px-4 py-24">
         <About language={language} />
+        </div>
       </ParallaxSection>
 
       {/* Events Section */}
-      <ParallaxSection className="bg-purple-50">
+      <ParallaxSection className="bg-white">
         <div className="max-w-7xl mx-auto px-4 py-24">
           <h2 className="text-4xl font-bold text-center mb-12 flex items-center justify-center">
             <Calendar className="mr-3" />
@@ -99,9 +50,8 @@ const HomePage = ({ language }) => {
       </ParallaxSection>
       <ParallaxSection className="bg-purple-50">
         <div className="max-w-7xl mx-auto px-4 py-24">
-          
           <Shlokas language={language} />
-          </div>
+        </div>
       </ParallaxSection>
       {/* Members Section */}
       <ParallaxSection className="bg-white">
